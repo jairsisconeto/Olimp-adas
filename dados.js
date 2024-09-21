a65f304b89e87a2c854729b15727aac7e5270cda
Jair Sisconeto <jairsisconeto@gmail.com>
2024-09-21 17:08:23 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil. Com seu estilo único e sua personalidade contagiante, Rayssa se tornou um ícone da nova geração de atletas e um símbolo de superação para meninas que sonham em praticar esportes.",
+      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+      tags: "skate fada sk8"
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Paris. Sua conquista histórica representou um marco para o judô feminino brasileiro e a inspirou uma nova geração de atletas. Com sua força e determinação, Beatriz se tornou um símbolo de superação e empoderamento para mulheres negras no esporte.",
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+      tags: "judô judo judoca judoka"
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com suas incríveis performances. Bicampeã olímpica, ela é considerada a maior medalhista da história do Brasil nos Jogos Olímpicos. Com sua força, agilidade e determinação, Rebeca se tornou um símbolo de superação e inspiração para jovens atletas em todo o país. Suas conquistas históricas no cenário internacional elevaram o patamar da ginástica artística brasileira e a consolidaram como uma das maiores atletas do mundo.",
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      tags:"ginástica ginastica ouro"
+    }
+  ];
